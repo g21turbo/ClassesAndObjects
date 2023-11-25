edfb6050380b5193b7e754883e4357219b859227
g21turbo <dg21stew@gmail.com>
2023-11-24 17:09:13 -0800

    Added objects

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 
         Car car = new Car();
-        car.setMake("Ford");
+        car.setMake("Toyota");
         car.setModel("Supra");
         car.setDoors(2);
         car.setConvertible(false);
@@ -13,6 +13,17 @@ public class Main {
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         car.describeCar();
+
+
+        Car coupe = new Car();
+        car.setMake("Honda");
+        car.setModel("NSX");
+        car.setDoors(2);
+        car.setConvertible(false);
+        car.setColor("Black");
+
+        coupe.describeCar();
+
     }
 
 }
